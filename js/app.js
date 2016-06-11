3f5575abc9d64ce0c2da20e33dddb475a4d66246
Vinicius Moraes <vsantana0212@gmail.com>
2016-06-11 17:58:03 -0400

    Correções estéticas na pesquisa.

--- js/app.js
+++ js/app.js
@@ -11,25 +11,26 @@ function initMap() {
     zoom: 15
   });
 
-  mapa.addListener('tilesloaded', function(){
-  //google.maps.event.addDomListener(window, 'load', function(){
+  // mapa.addListener('tilesloaded', function(){
+  google.maps.event.addDomListener(window, 'load', function(){
     var ne = mapa.getBounds().getNorthEast();
     var sw = mapa.getBounds().getSouthWest();
     if (urlBase.slice(-1) != "?"){
       urlBase = urlBase + "&";
     }
-    caminho = urlBase + "lat=" + ne.lat().toFixed(6) + "&lat=" + sw.lat().toFixed(6) + "&lng=" + ne.lng().toFixed(6) + "&lng=" + sw.lng().toFixed(6);
+    caminho = urlBase + "lat=" + ne.lat().toFixed(6) + "&lat=" + sw.lat().toFixed(6) +
+              "&lng=" + ne.lng().toFixed(6) + "&lng=" + sw.lng().toFixed(6);
     console.log(caminho);
 
     carregaOcorrencias(caminho);
   });
   //});
-
 }
 
-function adicionarOcorrencia (mapa, latLng){
+function adicionarOcorrencia (mapa, latLng, icone){
   var marcador = new google.maps.Marker({
     position: latLng,
+    icon: icone,
     map: mapa
   });
   arrayMarcadores.push(marcador);
@@ -42,33 +43,45 @@ function limpaMarcadores(){
   arrayMarcadores.length = 0;
 }
 
-$(document).ready(function(){
-})
-
-$("#botaoEnvio").click(function() {
-    var query = $("#formulario").serialize();
-    //if (query == []){
-      urlBase = "http://sigcao.herokuapp.com/ocorrencias/?"
-    //}
-    urlBase = urlBase + query;
-    console.log(urlBase);
-    carregaOcorrencias(urlBase);
-    return false;
-});
-
-
 function carregaOcorrencias(caminho){
-  limpaMarcadores();
   $.ajax({
     url: caminho
   }).then(function(data){
+    limpaMarcadores();
     for (i = 0; i < data.length; i++){
       var coordenadas = {
         lat: Number(data[i].latitude),
         lng: Number(data[i].longitude)
       };
-      adicionarOcorrencia(mapa, coordenadas);
+      var cores = ['yellow', 'blue', 'green', 'ltblue', 'orange', 'pink', 'purple', 'red'];
+      var icone = "https://maps.google.com/mapfiles/ms/icons/" + cores[data[i].doenca.id - 1] +  "-dot.png";
+      adicionarOcorrencia(mapa, coordenadas, icone);
     }
     console.log(arrayMarcadores.length);
   });
 }
+
+$body = $("body");
+
+$(document).on({
+    ajaxStart: function() {
+        $('#carregando').show();
+    },
+    ajaxStop: function() {
+        $('#carregando').hide();
+        $("#barra_busca").removeClass("is-visible");
+        $("#barra_busca").attr("aria-hidden", "true");
+        $(".mdl-layout__obfuscator").removeClass("is-visible");
+    }
+});
+
+$("#botaoEnvio").click(function() {
+    var query = $("#formulario").serialize();
+    urlBase = "http://sigcao.herokuapp.com/ocorrencias/?" + query;
+    carregaOcorrencias(urlBase);
+    return false;
+});
+
+$(document).ready(function(){
+    $('#carregando').hide();
+});
